05a4f9e5e6e755905511b3eba935b62372a3c054
zertu <290055513@qq.com>
2016-08-03 16:33:41 +0800

    123

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
+<<<<<<< b835196e5fd160fb1d165a768a6823ec85b84d57
 # nodechatroom
+=======
+﻿# Chatroom
+
+
+>>>>>>> nodejs实战第二章
---
+++ server.js
@@ -0,0 +1,54 @@
+﻿var http = require('http');
+var fs = require('fs');
+var path = require('path');
+var mime = require('mime');
+var cache = {};
+
+function send404(res) {
+    res.writeHead(404, { 'Content-type': 'text/plain' });
+    res.write('Error 404');
+    res.end();
+}//404页面
+
+function sendFile(res, filePath, fileContents) {
+    res.writeHead(200,
+        { 'Content-Type': mime.lookup(path.basename(filePath)) }
+    );
+res.end(fileContents);
+}//文件数据服务
+//先写出正确的http头然后发送文件的内容
+function serveStatic(res, cache, absPath) {
+    if (cache[absPath]) {//检查文件是否存在
+        sendFile(res, absPath, cache[absPath])//从内存返回文件
+    } else {
+        fs.exists(absPath, function (exists) {//文件是否存在
+            if (exists) {
+                fs.readFile(absPath, function (err, data) {//从硬盘中读文件
+                    if (err) {
+                        send404(res);
+                    }
+                    else {
+                        cache[absPath] = data;
+                        sendFile(res, absPath, data);//从硬盘中读取文件并返回
+                    }
+                });
+            } else {
+                send404(res);//如果读取失败返回404
+            }
+        })
+    }
+}
+var server = http.createServer(function (request, response) {
+    var filePath = false;
+    if (request.url == '/') {
+        filePath = 'public/index.html';
+    } else {
+        filePath = 'public' + request.url;
+    }
+    var absPath = './' + filePath;
+    serveStatic(response, cache, absPath);
+});
+server.listen(3000, function () {
+    // 终端打印如下信息
+    console.log('Success http://127.0.0.1:3000');
+});
