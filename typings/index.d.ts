0e48afae6b9c3d227406030f3d18e0066092990a
zertu <290055513@qq.com>
2016-08-04 00:08:16 +0800

    socket.io.js有问题

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,3 +1,4 @@
 /// <reference path="globals/mime/index.d.ts" />
 /// <reference path="globals/node/index.d.ts" />
+/// <reference path="globals/socket.io-client/index.d.ts" />
 /// <reference path="globals/socket.io/index.d.ts" />
