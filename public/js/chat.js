47a2f4fa247395bd494eac518f41916b3e563058
zertu <290055513@qq.com>
2016-08-03 23:54:35 +0800

    有个bug明天改把

---
+++ public/js/chat.js
@@ -0,0 +1,37 @@
+var Chat = function(socket){
+	this.socket=socket;
+}
+Chat.prototype.sendMessage=function(room,Text){
+	var message ={
+		room:room,
+		text:text
+	};
+	this.socket.emit('message',message);
+}//发送聊天消息
+Chat.prototype.changeRoom=function(room){
+	this.socket.emit('join',{//join用来加入
+		newRoom:room
+	});
+};
+//变更房间
+Chat.prototype.processCommand = function(command){
+	var words = command.split(' ');
+	var command =words[0].substring(1,words[0].length).toLowerCase();
+	var message =false;
+	switch(command){
+		case 'join':
+		words.shift();
+		var room =words.join(' ');
+		this.changeRoom(room);
+		break;
+		case 'nick':
+		words.shift();
+		var name =words.join(' ');
+		this.socket.emit('nameAttemt',name);
+		break;
+		default:
+		message='未知命令';
+		break;
+	}
+	return message;
+}
